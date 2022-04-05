0ed01d6a19425b02b521f1cb198efe0fa401589f
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2022-04-05 11:27:50 -0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Header from '@/components/header';
 import VideoSection from '@/components/video-section';
 import ListSection from '@/components/list-section';
 import FeatureSection from '@/components/feature-section';
-// import CasesSection from '@/components/cases-section';
 import SocialProof from '@/components/social-proof';
 import PricingTable from '@/components/pricing-table';
 import Footer from '@/components/footer';
